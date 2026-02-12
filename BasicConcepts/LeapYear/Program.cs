37a4c8529b49e8f988ee7cc0f4672e45be19a8a4
Samuel Aristizabal <samuaristi17@gmail.com>
2026-02-12 14:13:36 -0500

    LeapYear

---
+++ BasicConcepts/LeapYear/Program.cs
@@ -0,0 +1,20 @@
+﻿using Shared;
+
+do
+{
+    Console.WriteLine("Ingresa un año para determinar si es biciesto o no");
+    var year = ConsoleExtension.GetInt("Enter a year: ");
+    var num = 4;
+
+    if (year % num == 0)
+    {
+        Console.WriteLine($"El año {year}, es bisiesto");
+    }
+
+    else
+    { 
+        Console.WriteLine($"El año {year}, no es bisiesto");
+    }
+
+} while (true);
+Console.WriteLine("Fin del Juego");
