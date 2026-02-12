3f5c9767b0c5f516d949af53875976b8d1170138
Samuel Aristizabal <samuaristi17@gmail.com>
2026-02-11 23:08:34 -0500

    tercer ejercicio resuelto

---
+++ BasicConcepts/TheGreathestNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    Console.WriteLine("Ingrese tres numeros enteros diferentes para comparar cual es el mayor");
+    var a = ConsoleExtension.GetInt("Ingrese el primer numero: ");
+    var b = ConsoleExtension.GetInt("Ingrese el segundo numero: ");
+    var c = ConsoleExtension.GetInt("Ingrese el tercer numero: ");
+
+    if (a > b && a > c)
+    {
+        Console.WriteLine($"El numero mayor es: {a}");
+    }
+
+
+    else if (b > a && b > c)
+        {
+            Console.WriteLine($"El numero mayor es: {b}");
+        }
+
+    else
+    {
+        Console.WriteLine($"El numero mayor es: {c}");
+    }
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine("Game Over");
+
