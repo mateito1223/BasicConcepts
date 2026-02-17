388827936ddb5d8d400e0d6d37ab565fbc5c45e9
Samuel Aristizabal <samuaristi17@gmail.com>
2026-02-17 14:02:20 -0500

    SendMerchandise

--- BasicConcepts/RegistrationValue/Program.cs
+++ BasicConcepts/RegistrationValue/Program.cs
@@ -1,1 +1,72 @@
-﻿
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+
+    var credits = ConsoleExtension.GetInt("Ingrese el numero de creditos: ");
+    var CreditValue = ConsoleExtension.GetDecimal("Ingrese el valor del credito: ");
+    var stratum = ConsoleExtension.GetInt("Estrato del estudiante: ");
+
+    var RegistrationValue = CalculateRegistrationValue(credits, CreditValue, stratum);
+    var subsidy = CalculateSubsidy(stratum);
+
+    Console.WriteLine($"El valor a pagar por la matricula es: {(RegistrationValue),20:C2}");
+    Console.WriteLine($"El subsidio otorgado es: {(subsidy),20:C2}");
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Desea Continuar? [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine("Game Over");
+
+decimal CalculateSubsidy(int stratum)
+{
+    if (stratum == 1)
+    {
+        return 200000m;
+    }
+
+    if (stratum == 2)
+    {
+        return 100000m;
+    }
+
+    return 0;
+}
+
+
+decimal CalculateRegistrationValue(int credits, decimal CreditValue, int stratum)
+{
+    decimal value;
+    if (credits <= 20)
+    {
+        value = credits * CreditValue;
+    }
+
+    else
+    {
+        value = 20 * CreditValue + (credits - 20) * CreditValue * 2;
+    }
+
+    if (stratum == 1)
+    {
+        return value * 0.20m;
+    }
+
+    if (stratum == 2)
+    {
+        return value * 0.50m;
+    }
+
+    if (stratum == 3)
+    {
+        return value * 0.70m;
+    }
+
+    return value;
+}
---
+++ BasicConcepts/SendMerchandise/Program.cs
@@ -0,0 +1,107 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    int weight = ConsoleExtension.GetInt("Ingrese el peso: ");
+    var Value = ConsoleExtension.GetDecimal("Ingrese el valor de la mercancia: ");
+    String monday;
+
+
+    do
+    {
+        monday = ConsoleExtension.GetValidOptions("¿Es lunes? [S]i, [N]o: ", options)!;
+    } while (!options.Any(x => x.Equals(monday, StringComparison.CurrentCultureIgnoreCase)));
+
+
+    var paymentType = new List<string> { "e", "t" };
+    string payMethod;
+    do
+    {
+        payMethod = ConsoleExtension.GetValidOptions("Tipo de pago [E]fectivo [T]arjeta: ", paymentType)!;
+    } while (!paymentType.Any(x => x.Equals(payMethod, StringComparison.CurrentCultureIgnoreCase)));
+
+
+    var fare = CalculateFare(weight);
+
+    decimal promotion = CalculatePromotion(fare, monday, payMethod, Value);
+    decimal Discount = 0;
+
+    if (promotion == 0)
+    {
+        Discount = CalculateDiscount(fare, Value);
+    }
+
+    Console.WriteLine($"Tarifa: {(fare),51:C2}");
+    Console.WriteLine($"Descuento: {(Discount),48:C2}");
+    Console.WriteLine($"Promocion: {(promotion),48:C2}");
+    Console.WriteLine($"El valor total a pagar es de: {(fare - Discount - promotion),29:C2}");
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Desea Continuar? [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine("Game Over");
+
+
+decimal CalculatePromotion(decimal fare, string monday, string payMethod, decimal Value)
+{
+    if (monday.ToLower() == "s" && payMethod.ToLower() == "t")
+    {
+        return fare * 0.50m;
+    }
+
+    if (payMethod.ToLower() == "e" && Value > 1000000m)       
+    { 
+      return fare * 0.40m;
+    }
+
+    return 0;
+}
+
+
+decimal CalculateDiscount(decimal fare, decimal Value)
+{
+    if (Value >= 300000m && Value <= 600000m)
+    {
+        return fare * 0.10m;
+    }
+    if (Value > 600000m && Value <= 1000000m)
+    {
+        return fare * 0.20m;
+    }
+    if (Value > 1000000m)
+    {
+        return fare * 0.30m;
+    }
+    return 0;
+}
+
+
+decimal CalculateFare(int weight)
+{
+    if (weight < 100)
+    {
+        return 20000m;
+    }
+    if (weight >= 100 && weight <= 150)
+    {
+        return 25000m;
+    }
+    if (weight > 150 && weight <= 200)
+    {
+        return 30000m;
+    }
+    if (weight > 200)
+    {
+        return 35000m + ((weight - 200) / 10) * 2000m;
+    }
+    return 0;
+}
+
+
+
