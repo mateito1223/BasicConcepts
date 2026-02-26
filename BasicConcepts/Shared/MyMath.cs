7867320b5773798fdf53542b7954235523a25b83
Samuel Aristizabal <samuaristi17@gmail.com>
2026-02-26 15:37:01 -0500

    FactorialNum

---
+++ BasicConcepts/Shared/MyMath.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Shared
+{
+    public class MyMath
+    {
+        public static double Factorial(int n)
+        {
+        double factorial = 1;
+        for (int i = 2; i <= n; i++)
+         {
+            factorial *= i;
+         }
+        return factorial;
+        }
+    }   
+}
+
