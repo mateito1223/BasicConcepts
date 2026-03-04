ea4cfb63686d8660a75cef3eef4d90a71b0ff50b
Samuel Aristizabal <samuaristi17@gmail.com>
2026-03-04 18:08:17 -0500

    Fibonacci and FibonacciModified

---
+++ BasicConcepts/FibonacciModified/Program.cs
@@ -0,0 +1,45 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var n = ConsoleExtension.GetInt("Cuantos terminos quiere: ");
+    double a = 0;
+    double b = 1;
+    double c = 2;
+
+    if (n == 1)
+    {
+        Console.WriteLine($"{a}");
+    }
+    else if (n == 2)
+    {
+        Console.WriteLine($"{a}\t{b}");
+    }
+    else if (n == 3)
+    {
+        Console.WriteLine($"{a}\t{b}\t{c}\t");
+    }
+    else
+    {
+        Console.Write($"{a}\t{b}\t{c}\t");
+        for (int i = 4; i <= n; i++)
+        {
+            double d = a + b + c;
+            Console.Write($"{d}\t");
+            a = b;
+            b = c;
+            c = d;
+        }
+        Console.WriteLine();
+    }
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine("Game Over");
---
+++ BasicConcepts/SerieFibonacci/Program.cs
@@ -0,0 +1,39 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var n = ConsoleExtension.GetInt("Cuantos terminos quiere: ");
+    double a = 0;
+    double b = 1;
+
+    if (n == 1)
+    { 
+    Console.WriteLine($"{a}");
+    }
+    else if (n == 2)
+    {
+        Console.WriteLine($"{a}\t{b}");
+    }
+    else
+    {
+        Console.Write($"{a}\t{b}\t");
+        for (int i = 3; i <= n; i++)
+        {
+            double c = a + b;
+            Console.Write($"{c}\t");
+            a = b;
+            b = c;
+        }
+        Console.WriteLine();
+    }
+    
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine("Game Over");
