357595bc9ca5ce0a3d5a01066fd36ae6a8789843
Samuel Aristizabal <samuaristi17@gmail.com>
2026-03-10 15:16:19 -0500

    PrimeNumbers and eNumber

---
+++ BasicConcepts/PrimeNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using Shared;
+using System.IO.Pipelines;
+
+var options = new List<string> {"s", "n"};
+var answer = string.Empty;
+
+do
+{
+	var n = ConsoleExtension.GetInt("¿Cuantos numeros deseas?: ");
+	var primes = GetPrimes(n);
+	foreach (var prime in primes)
+	{
+		Console.Write($"{prime,10:N0}");
+	}
+	Console.WriteLine();
+    Console.WriteLine($"La sumatoria es: {primes.Sum(),5:N0}");
+    Console.WriteLine($"El promedio  es: {primes.Average(),5:N0}");
+    Console.WriteLine();
+
+    do
+	{
+		answer = ConsoleExtension.GetValidOptions("Desea continuar (s/n)", options);
+		Console.WriteLine();
+	} while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+List<int> GetPrimes(int n)
+{
+	var primes = new List<int>();
+	var num = 2;
+	while (primes.Count < n)
+	{
+		if (MyMath.IsPrime(num))
+		{
+			primes.Add(num);
+		}
+		num++;
+	}
+	return primes;
+}
---
+++ BasicConcepts/eNumber/Program.cs
@@ -0,0 +1,42 @@
+﻿using Shared;
+
+var options = new List<string> { "s", "n" };
+var answer = string.Empty;
+
+do
+{
+    // Data input
+    Console.BackgroundColor = ConsoleColor.Blue;
+    Console.Clear();
+    Console.WriteLine("CALCULO DEL NUMERO E");
+    var n = ConsoleExtension.GetInt("Cuantos terminos deseas procesar: ");
+
+    // Do process
+    var e = CalculateE(n);
+    // Show results
+    Console.BackgroundColor = ConsoleColor.Black;
+    Console.ForegroundColor = ConsoleColor.Yellow;
+
+    Console.WriteLine($"El valor de E con {n} terminos de precision es: {e}");
+
+    Console.BackgroundColor = ConsoleColor.Blue;
+    Console.ForegroundColor = ConsoleColor.White;
+
+
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("Desea continuar (s/n)", options);
+        Console.WriteLine();
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+double CalculateE(int n)
+{
+    double sum = 0;
+    for (int i = 0; i < n; i++)
+    { 
+    sum += 1 / MyMath.Factorial(i);
+    }
+    return sum;
+}
